de696f28ff3b8c4ac8b843bc9b864d872a1134b9
shubhamChauhan <shubhamchauhan4488@gmail.com>
2018-06-10 01:41:41 +0530

    learning basics

--- Indecision app/src/playground/es6-arrowFunctions.js
+++ Indecision app/src/playground/es6-arrowFunctions.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // //Functions ES syntax 
 // function square(x){
 //  return x*x; //returns the square of the number
@@ -30,28 +28,26 @@
 
 
 //Es 5 arguments object is no longer used in ES6 
-var add = function add(a, b) {
+const add = function(a,b){
     console.log(arguments);
-    return a + b;
-};
-console.log(add(3, 4, 5));
+    return a+b
+}
+console.log(add(3,4,5))
 
 //ES6 syntax , arguments no longer available
-var add2 = function add2(a, b) {
+const add2 = (a,b) => {
     // console.log(arguments);
-    return a + b;
-};
-console.log(add2(3, 4, 5));
+    return a+b
+}
+console.log(add2(3,4,5))
 
 //ES5 this was not bound
-var user = {
-    name: 'shubham',
-    cities: ['delhi', 'mumbai', 'toronto'],
-    citiesVisited: function citiesVisited() {
-        var _this = this;
-
-        this.cities.forEach(function (city) {
-            console.log(_this.name + ' visited ' + city);
+const user = {
+    name : 'shubham',
+    cities : ['delhi','mumbai','toronto'],
+    citiesVisited : function(){
+        this.cities.forEach((city) => {
+            console.log(this.name + ' visited ' + city);
             //'this' here (in arrow function will use the this  of the parent i.e this of this.cities, so no need to bound 'this' as in the case of ES5 )
         });
     }
